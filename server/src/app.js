2f863998aa9d3097406c579b83956f030e302a8e
Ivey Topaz <iveytopaz@gmail.com>
2018-05-16 12:48:17 +0100

    Remove auto mocks

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,13 @@
-/* eslint-disable import/first */
 // Dotenv is only used in development
 import './env';
+import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
+import bodyParser from 'body-parser';
 import config from 'config';
 import debugModule from 'debug';
 import express from 'express';
 import morgan from 'morgan-debug';
 import path from 'path';
-import bodyParser from 'body-parser';
-import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
-import { addMockFunctionsToSchema } from 'graphql-tools';
 import schema from './graphql/schema';
-import mocks from './graphql/mocks/resolvers';
 
 const { NODE_ENV } = process.env;
 const isDevelopment = NODE_ENV === 'development';
@@ -19,10 +16,6 @@ const app = express();
 const debug = debugModule('server:http');
 const port = config.get('serverPort');
 const logFormat = isDevelopment ? 'dev' : 'combined';
-
-if (isDevelopment) {
-  addMockFunctionsToSchema({ schema, mocks });
-}
 
 app.use(morgan(debug.namespace, logFormat));
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
