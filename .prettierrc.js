c0c0f3822ed84a172dd8e6ad7b665822df111824
Ivey Topaz <iveytopaz@gmail.com>
2018-05-16 12:47:16 +0100

    Add global prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+// Shared prettier config for client and server
+
+module.exports = {
+  printWidth: 120,
+  singleQuote: true,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  bracketSpacing: true
+};
