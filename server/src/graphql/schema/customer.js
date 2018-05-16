c284fe010ecd0afd022c4ec869075469799b1a36
Ivey Topaz <iveytopaz@gmail.com>
2018-05-16 13:17:35 +0100

    Use graphql-js

--- server/src/graphql/schema/customer.js
+++ server/src/graphql/schema/customer.js
@@ -1,38 +1,43 @@
-import gql from '../syntaxHelpers';
-import Order from './order';
+import { globalIdField, connectionFromArray, connectionArgs, connectionDefinitions } from 'graphql-relay';
+import { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLNonNull, GraphQLBoolean, GraphQLID } from 'graphql';
+import { getOrdersByCustomer } from '../resolvers';
+import { OrderConnection } from './order';
+import { zNodeInterface } from './zNode';
 
-const Customer = gql`
-  type Name {
-    first: String
-    last: String
-  }
+const NameType = new GraphQLObjectType({
+  name: 'Name',
+  fields: () => ({
+    first: { type: GraphQLString },
+    last: { type: GraphQLString }
+  })
+});
 
-  type Customer implements Node {
-    id: ID!
-    index: Int
-    guid: String!
-    isActive: Boolean
-    age: Int
-    name: Name
-    orders: [Order]
-  }
+const CustomerType = new GraphQLObjectType({
+  name: 'Customer',
+  description: 'A Bravisismo customer',
+  fields: () => ({
+    id: globalIdField('Customer', customer => customer.guid),
+    index: { type: GraphQLInt },
+    guid: { type: new GraphQLNonNull(GraphQLID) },
+    isActive: { type: GraphQLBoolean },
+    age: { type: GraphQLInt },
+    name: { type: NameType },
+    orders: {
+      type: OrderConnection,
+      description: "A customer's orders",
+      args: connectionArgs,
+      resolve: (customer, args) => connectionFromArray(getOrdersByCustomer(customer.guid), args)
+    }
+  }),
+  interfaces: [zNodeInterface],
+  isTypeOf: obj => !!obj.name
+});
 
-  type CustomerEdge {
-    cursor: String!
-    node: Customer
-  }
+const { connectionType: CustomerConnection } = connectionDefinitions({ nodeType: CustomerType });
 
-  type CustomerConnection {
-    edges: [CustomerEdge]
-    pageInfo: PageInfo!
-  }
-
-  type PageInfo {
-    hasNextPage: Boolean!
-    hasPreviousPage: Boolean!
-    startCursor: String
-    endCursor: String
-  }
-`;
-
-export default () => [Customer, Order];
+export default {
+  // Exporting as function to avoid issue with circular dependency between node type,
+  // and types that implement node
+  CustomerType,
+  CustomerConnection
+};
