de0130f7045da690d4c5711a187a57b8aa5ff3b0
Ivey Topaz <iveytopaz@gmail.com>
2018-05-18 13:09:49 +0100

    Add a viewer

--- server/src/graphql/schema/index.js
+++ server/src/graphql/schema/index.js
@@ -4,26 +4,42 @@ import { getCustomer, getCustomers } from '../resolvers';
 import { GraphQLSchema, GraphQLObjectType, GraphQLID } from 'graphql';
 import { zNodeField } from './zNode';
 
+const Customer = {
+  type: CustomerType,
+  args: {
+    id: { type: GraphQLID }
+  },
+  resolve: (_, { id }) => getCustomer(id)
+};
+
+const Customers = {
+  type: CustomerConnection,
+  args: connectionArgs,
+  resolve: (_, args) => connectionFromArray(getCustomers(), args)
+};
+
+const ViewerType = new GraphQLObjectType({
+  name: 'Viewer',
+  fields: () => ({
+    customer: Customer,
+    customers: Customers
+  })
+});
+
 const RootQuery = new GraphQLObjectType({
   name: 'RootQuery',
   fields: () => ({
-    customer: {
-      type: CustomerType,
-      args: {
-        id: { type: GraphQLID }
-      },
-      resolve: (_, { id }) => getCustomer(id)
-    },
-    customers: {
-      type: CustomerConnection,
-      args: connectionArgs,
-      resolve: (_, args) => connectionFromArray(getCustomers(), args)
+    Customer,
+    Customers,
+    viewer: {
+      type: ViewerType,
+      resolve: viewer => ({ viewer })
     },
     node: zNodeField
   })
 });
 
-// const RootMutation = gql`
+// const RootMutation = `
 //   input ChangeNameInput {
 //     clientMutationId: String!
 //     guid: String!
